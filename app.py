a55cab148bc3e1cf0efd705dde4ca7dbb1814153
dolgushinabv.29 <bogdanadolgusina@gmail.com>
2026-01-22 18:28:21 +0300

    гифка

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, request
 
 app = Flask(__name__)
 
@@ -6,5 +6,18 @@ app = Flask(__name__)
 def home():
     return render_template("test.html")
 
+
+@app.route('/', methods=['POST'])
+def page():
+    print(239)
+    if request.method == 'POST':
+        return redirect(url_for('newPage'))
+
+@app.route('/newPage')
+def newPage():
+    return render_template("newPage.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+    # return render_template("newPage.html")
