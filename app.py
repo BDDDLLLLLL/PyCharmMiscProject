7a83d6fb8e03c280f636a8bd82bc4f7fcde7d4f6
dolgushinabv.29 <bogdanadolgusina@gmail.com>
2026-01-29 18:29:32 +0300

    видеопоток фильтр гифка видео кнопка для перемещения с одной страницы на другую утка без фона бегает

--- app.py
+++ app.py
@@ -1,23 +1,52 @@
+#import random
+#from flask import Flask, render_template, url_for, redirect, request
+
+#app = Flask(__name__)
+
+#@app.route('/')
+#def home():
+#    return render_template("test.html")
+
+
+
+#@app.route('/', methods=['POST'])
+#def page():
+  #  print(239)
+  #  if request.method == 'POST':
+  #      return redirect(url_for('newPage'))
+
+#@app.route('/newPage')
+#def newPage():
+   # return render_template("newPage.html")
+
+#if __name__ == '__main__':
+  #  app.run(debug=True)
+
+    # return render_template("newPage.html")
+import random
 from flask import Flask, render_template, url_for, redirect, request
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])  # Разрешаем оба метода здесь
 def home():
-    return render_template("test.html")
-
-
-@app.route('/', methods=['POST'])
-def page():
-    print(239)
     if request.method == 'POST':
+        print(239)
         return redirect(url_for('newPage'))
+
+    # Если это обычный заход на страницу (GET):
+    random_path = [
+        {'x': random.randint(0, 85), 'y': random.randint(0, 85)}
+        for _ in range(5)
+    ]
+    return render_template("test.html", path=random_path)
+
 
 @app.route('/newPage')
 def newPage():
     return render_template("newPage.html")
 
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-    # return render_template("newPage.html")
