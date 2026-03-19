a389c4859ca8cec3b0a4d73cfec10ca0285b0cba
dolgushinabv.29 <bogdanadolgusina@gmail.com>
2026-03-19 18:28:55 +0300

    СДЕЛАТЬ ЧТОБЫ ЧЕЛОВЕЧЕК НОРМАЛЬНО БЕГАЛ

--- labbebe.py
+++ labbebe.py
@@ -3,10 +3,15 @@ import timeit
 import random
 import pygame
 screen=pygame.display.set_mode((375, 625))
+
+
 n=25 #строк
 m=15 #столбцов
 x=0
 y=0
+r1= True
+x1=-1
+y1=-1
 mas = [
     [1,1,0,0,0,0,0,1,0,0,0,0,0,0,0],
     [0,1,0,0,0,1,1,1,0,1,1,1,1,1,1],
@@ -39,10 +44,15 @@ mas = [
 
 
 ]
+
+timerrr= 2
 size=25
 stena=pygame.image.load("stena.png")
 doroga=pygame.image.load("doroga.png")
-utka=pygame.image.load("utkalab.png")
+pizza=pygame.image.load("pizza.png")
+ch=pygame.image.load("chillguy.png")
+my_time = pygame.time.set_timer(timerrr,5000,10)
+
 while 1:
 
 
@@ -53,29 +63,60 @@ while 1:
             if mas[i][j] == 0:
                 screen.blit(stena,(j*size,i*size))
 
-    for i in range(0,n):
-        for j in range(0,m):
-            if mas[i][j]==1:
-                for event in pygame.event.get():
+
+
+    for event in pygame.event.get():
+                    if event.type == timerrr:
+                        print("timer!")
+                        x1=0
+                        y1=0
                     if event.type == pygame.KEYDOWN:
                         if event.key == pygame.K_DOWN:
-                            y += 25
-                            if y>625:
-                                y=600
+                            if y < 625-25:
+                                if mas[y//25+1][x//25] == 1:
+                                    y += 25
+
                         if event.key==pygame.K_UP:
-                            y-=25
-                            if y<0:
-                                y=0
+                            if y > 0:
+                                if mas[y//25-1][x//25] == 1:
+                                    y-=25
                         if event.key==pygame.K_LEFT:
-                            x-=25
-                            if x<0:
-                                x=0
+                            if x > 0:
+                                if mas[y//25][x//25-1]==1:
+                                    x-=25
+
 
                         if event.key==pygame.K_RIGHT:
-                            x+=25
-                            if x > 375:
-                                x=350
-            else:
-                y+=0
-    screen.blit(utka, (x, y))
+                            if x < 375-25:
+                                if mas[y//25][x//25+1] == 1:
+                                    x+=25
+
+                    else:
+                        y+=0
+
+    if x1>-10:
+        x1+=1
+        y1+=1
+        if x1>400:
+            y1=600
+            x1-=1
+            y1-=1
+            if y1>625:
+                y1=-10
+                x1-=1
+                y1-=1
+                if y1<-10:
+                    y1=200
+                    y1+=1
+                    x1+=1
+
+
+    print(x1,y1)
+    m1 = pygame.mask.from_surface(pizza)
+    m2 = pygame.mask.from_surface(ch)
+    of1 = (x1 - x, y1 - y)
+    if m1.overlap(m2, of1):
+        print("you lost")
+    screen.blit(pizza, (x, y))
+    screen.blit(ch,(x1,y1))
     pygame.display.update()
